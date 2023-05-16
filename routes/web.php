832ff4ba91351f49bc327be14cb981570ea907ed
Ryckson Diego <oliveira.diego@escolar.ifrn.edu.br>
2023-05-16 14:15:51 -0300

    Primeiro Commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return '<h1>{“hello_url” : “/hello”}</h1>';
+});
+Route::get('/hello', function () {
+    return '<h1>Hello, World!</h1>';
+});
